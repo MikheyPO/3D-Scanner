b5dce4f534f39982bdc851b2ccfbaa72e4cc68ce
MikheyPO <mikhey.work@gmail.com>
2022-11-18 17:43:45 +0100

    init

---
+++ Scanner 3D/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scanner 3D
+//
+//  Created by Vladislav Mikheenko on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
